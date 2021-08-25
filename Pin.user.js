f2300e4a3aa362e005ae893d019f146eb1b3134a
DesignBun <89509272+DesignBun@users.noreply.github.com>
2021-08-25 10:57:35 +0300

    03

--- Pin.user.js
+++ Pin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pin
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.pinterest.ru/pin-builder/*
@@ -19,5 +19,6 @@ const title="111" // Название альбома куда сохранять
             elements.click();
         } else {
             console.log('Нету элемента');
+            alert(`Что то пошло не так, отключите скрипт`);
         }
     }, Math.random() * 5000 + 5000);
